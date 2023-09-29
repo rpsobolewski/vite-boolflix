8e1d4009658c46b4485f122a18b156ad72bae781
rpsobolewski <rpsobolewski@gmail.com>
2023-09-29 13:31:59 +0200

    work on flags

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <script >
+
 import { state } from './components/state'
 
 export default {
@@ -7,6 +8,7 @@ export default {
     return {
       state,
       searchQuery: "",
+      lingue: ['en', 'it', 'es', 'fr']
     }
   },
 
@@ -33,8 +35,15 @@ export default {
       <div class="card_data text-center">
         <h1>titolo: {{ film.title }}</h1>
         <h2> titolo originale: {{ film.original_title }}</h2>
-        <span v-if="film.original_language != 'en'">lingua: {{ film.original_language }}</span>
+        <span v-if="!lingue.includes(film.original_language)">lingua: {{ film.original_language
+        }}</span>
         <img class="flag" src="../src/img/british.png" alt="en" v-if="film.original_language == 'en'">
+        <img class="flag" src="../src/img/italian.png" alt="en" v-if="film.original_language == 'it'">
+        <img class="flag" src="../src/img/spanish.png" alt="en" v-if="film.original_language == 'es'">
+        <img class="flag" src="../src/img/french.png" alt="en" v-if="film.original_language == 'fr'">
+
+
+
         <div>voto: {{ film.vote_average }}</div>
       </div>
     </div>
@@ -44,5 +53,6 @@ export default {
 <style scoped>
 .flag {
   height: 20px;
+  aspect-ratio: 3/2;
 }
 </style>
