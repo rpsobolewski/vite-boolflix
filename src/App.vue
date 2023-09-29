820e02aaa2c3f8fa4344c259cf6f57eb32efc1eb
rpsobolewski <rpsobolewski@gmail.com>
2023-09-29 16:36:18 +0200

    work on series

--- src/App.vue
+++ src/App.vue
@@ -8,15 +8,18 @@ export default {
     return {
       state,
       searchQuery: "",
-      lingue: ['en', 'it', 'es', 'fr']
+      lingue: ['en', 'it', 'es', 'fr'],
+
     }
   },
 
   methods: {
     searchMovies() {
       console.log("Search Query:", this.searchQuery);
+
       state.fetchFilms();
     },
+
   },
 
 }
@@ -26,15 +29,18 @@ export default {
 
 <template>
   <input type="text" v-model="state.searchQuery" placeholder="Inserisci un titolo">
-  <button @click="searchMovies">Search</button>
+  <button @click="searchMovies">Search </button>
 
-  <div v-for=" film  in   state.films  ">
+
+
+  <div v-for=" film  in   state.films  "><!-- film -->
     <div class=" card h-100">
 
 
       <div class="card_data text-center">
         <h1>titolo: {{ film.title }}</h1>
-        <h2> titolo originale: {{ film.original_title }}</h2>
+        <h2>titolo originale: {{ film.original_title }}</h2>
+
         <span v-if="!lingue.includes(film.original_language)">lingua: {{ film.original_language
         }}</span>
         <img class="flag" src="../src/img/british.png" alt="en" v-if="film.original_language == 'en'">
@@ -45,6 +51,27 @@ export default {
 
 
         <div>voto: {{ film.vote_average }}</div>
+      </div>
+    </div>
+  </div>
+  <div v-for=" serie  in   state.series  "><!-- serie -->
+    <div class=" card h-100">
+
+
+      <div class="card_data text-center">
+        <h1>titolo: {{ serie.name }}</h1>
+
+
+        <span v-if="!lingue.includes(serie.original_language)">lingua: {{ serie.original_language
+        }}</span>
+        <img class="flag" src="../src/img/british.png" alt="en" v-if="film.original_language == 'en'">
+        <img class="flag" src="../src/img/italian.png" alt="en" v-if="film.original_language == 'it'">
+        <img class="flag" src="../src/img/spanish.png" alt="en" v-if="film.original_language == 'es'">
+        <img class="flag" src="../src/img/french.png" alt="en" v-if="film.original_language == 'fr'">
+
+
+
+        <div>voto: {{ serie.vote_average }}</div>
       </div>
     </div>
   </div>
--- src/components/state.js
+++ src/components/state.js
@@ -2,9 +2,11 @@ import axios from "axios";
 import { reactive } from "vue";
 export const state = reactive({
   films: null,
+  series: null,
 
   fetchFilms() {
     let base_url = "https://api.themoviedb.org/3/search/movie";
+
     console.log("Search Query:", this.searchQuery);
     axios
       .get(base_url, {
@@ -18,6 +20,26 @@ export const state = reactive({
         console.log(response);
         this.films = response.data.results;
         console.log(this.films);
+      });
+  },
+
+  fetchFilms() {
+    let tv_url = "https://api.themoviedb.org/3/search/tv";
+
+    console.log("Search Query:", this.searchQuery);
+
+    axios
+      .get(tv_url, {
+        params: {
+          api_key: "f72277928616c6e9caa7f4df2963dd14",
+          query: this.searchQuery,
+        },
+      })
+
+      .then((response) => {
+        console.log(response);
+        this.series = response.data.results;
+        console.log(this.series);
       });
   },
 });
